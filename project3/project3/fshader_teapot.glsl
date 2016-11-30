1dc7566670f8e147e7bf65b7f64b1bcd0e096e0d
laciecochran <cochralr@email.sc.edu>
2016-11-30 13:02:57 -0500

    started working onusing an image as a pattern

--- project3/project3/fshader_teapot.glsl
+++ project3/project3/fshader_teapot.glsl
@@ -5,10 +5,11 @@
 in vec4 color;
 in vec2 texCoord;
 
-uniform sampler2D tex; 
+uniform sampler2D tex1;
+uniform sampler2D tex2; 
 
 void main() 
 { 
-    gl_FragColor = color * texture( tex, texCoord );
+    gl_FragColor = color * texture( tex1, texCoord ) * texture( tex2, texCoord );
 } 
 
