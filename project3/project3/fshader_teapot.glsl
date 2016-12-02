8125d42c7ef5bb7d5418350870ce61c39fe723ed
laciecochran <cochralr@email.sc.edu>
2016-12-02 18:33:43 -0500

    finished project

--- project3/project3/fshader_teapot.glsl
+++ project3/project3/fshader_teapot.glsl
@@ -4,19 +4,15 @@
 //out vec4 fColor;
 in vec4 color;
 in vec2 texCoord;
-in vec3 light;
-in vec3 view;
+in vec3 R;
 
-uniform sampler2D tex1;
+uniform samplerCube tex1;
 uniform sampler2D tex2;
+uniform sampler2D tex3;
 uniform vec4 DiffuseProduct;
 
 void main() 
 { 
-    vec4 N = texture( tex1, texCoord ) * texture( tex2, texCoord );
-    vec3 NN = normalize(2.0*N.xyz-1.0);
-    vec3 LL = normalize(light);
-    float Kd = max(dot(NN.xyz, LL), 0.0);
-    gl_FragColor = color * Kd * DiffuseProduct;
+    gl_FragColor = color * texture( tex1, R ) * texture(tex2, texCoord) * texture(tex3, texCoord);
 } 
 
