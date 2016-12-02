bbce6f5604a94edc3fddd69ed333d44e6e349f26
laciecochran <cochralr@email.sc.edu>
2016-12-02 16:28:09 -0500

    started bump mapping

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # CSCE_565
+Authors: Lacie Cochran
+	 Madushan Abeysinghe
+
+To satisfy the graduate student requirement, we bound the multi-texture to key 3.
+	Key 1 is a checkerboard pattern,
+	Key 2 is a spotted pattern,
+	Key 3 is a mixture of both the checkerboard and the spots.
--- project3/project3/fshader_teapot.glsl
+++ project3/project3/fshader_teapot.glsl
@@ -1,15 +1,22 @@
-#version 150 
+#version 150 core
 
 
 //out vec4 fColor;
 in vec4 color;
 in vec2 texCoord;
+in vec3 light;
+in vec3 view;
 
 uniform sampler2D tex1;
-uniform sampler2D tex2; 
+uniform sampler2D tex2;
+uniform vec4 DiffuseProduct;
 
 void main() 
 { 
-    gl_FragColor = color * texture( tex1, texCoord ) * texture( tex2, texCoord );
+    vec4 N = texture( tex1, texCoord ) * texture( tex2, texCoord );
+    vec3 NN = normalize(2.0*N.xyz-1.0);
+    vec3 LL = normalize(light);
+    float Kd = max(dot(NN.xyz, LL), 0.0);
+    gl_FragColor = color * Kd * DiffuseProduct;
 } 
 
